ce1cc58d704666d6e99ac17dfbab69d262cdfcff
akycyel <coreiadosul09@gmail.com>
2025-04-11 22:26:24 -0300

    Add files via upload

--- onixx.py
+++ onixx.py
@@ -1,37 +1,10 @@
-
 import pexpect
 import requests
-
 import pyfiglet
-
 import shlex
 import subprocess
 import os
 import time
-# Verificação de sistema e de login para novos usuários
-def instalar_books():
-	os.system('pip install pyfiglet')
-	os.system('pip install beautifulsoup4')
-	os.system('pkg install nmap')
-	os.system('pip install requests')
-	os.system('pip install pexpect')
-	
-
-def verificar_pacotes():
-	clear_()
-	texto_d('\033[1;37mVERIFICANDO PACOTES...\033[m\n')
-	
-	pacotes_do_onixx = ['pyfiglet', 'beautifulsoup4', 'nmap', 'requests', 'pexpect']
-	terminal = subprocess.run(['pip', 'list'], capture_output=True, text=True)
-	for c in range(0, 5):
-		time.sleep(1)
-		print('\033[1;33mVerificando', pacotes_do_onixx[c], '\033[m')
-		if pacotes_do_onixx[c] not in terminal.stdout:
-			print('\033[1;31mPacote', pacotes_do_onixx[c], 'não instalado\033[m' )
-			system.os('pip install ', pacotes_do_onixx[c])
-		else:
-			print('\033[1;32mPacote', pacotes_do_onixx[c], 'instalado\033[m')
-
 
 # Daqui pra baixo onde as parada acontece
 
@@ -561,7 +534,6 @@ def menu_principal():
 		
 		
 		
-verificar_pacotes()		
 menu_principal()
 	
 		
